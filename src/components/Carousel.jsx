cb53062d333873d587891de82cf30d02314c5c9f
ayushtechie <techie.ayushtiwari@gmail.com>
2023-12-10 08:59:04 +0530

    add images in folder

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,10 @@
 // Default theme
 import '@splidejs/react-splide/css';
 
+import solar1 from "../images/solar1.jpg";
+import solar11 from "../images/solar11.jpg";
+import solar111 from "../images/solar111.jpg";
+
 // or only core styles
 import '@splidejs/react-splide/css/core';
 import { Splide, SplideSlide } from "@splidejs/react-splide";
@@ -16,13 +20,13 @@ export default function Carousel() {
         height: '90vh',
         }} className='p-0'>
       <SplideSlide>
-        <img src='solar11.jpg' alt="Image1" className='h-full w-full' />
+        <img src={solar11} alt="Image1" className='h-full w-full' />
       </SplideSlide>
       <SplideSlide>
-        <img src="solar111.jpg" alt="Image2" className='h-full w-full' />
+        <img src={solar111} alt="Image2" className='h-full w-full' />
       </SplideSlide>
       <SplideSlide>
-        <img src="solar1.jpg" alt="Image2" className='h-full w-full' />
+        <img src={solar1} alt="Image2" className='h-full w-full' />
       </SplideSlide>
     </Splide>
   );
--- src/components/Estimator.jsx
+++ src/components/Estimator.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Button } from "@material-tailwind/react";
 
+import residential from "../images/residential.png"
+
 const pincodeMapping = {
     "201301": "Noida",
     "110001": "Delhi",
@@ -65,7 +67,7 @@ export default function Estimator() {
                 d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
               />
             </svg> */}
-            <img src="residential.png" alt="residential" className="w-9 h-9" />
+            <img src={residential} alt="residential" className="w-9 h-9" />
             <p className="font-bold text-md sm:text-xl text-blue-700">Residential</p>
           </div>
 
