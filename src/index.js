95e359ab1023a697c33da99f478e7e4f4130fceb
ayushtechie <techie.ayushtiwari@gmail.com>
2024-01-21 12:19:44 +0530

    update

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <HashRouter>
+      
         <App />
-      </HashRouter>
+      
     </ThemeProvider>
   </React.StrictMode>
 );
