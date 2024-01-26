4b3f781f7b9a4fe798eedb01f4a664e6f8b9a414
ayushtechie <techie.ayushtiwari@gmail.com>
2024-01-27 01:34:08 +0530

    solar modules setup

--- src/pages/SolarModule.jsx
+++ src/pages/SolarModule.jsx
@@ -9,19 +9,24 @@ const SolarModule = () => {
     <>
       <div className="flex items-center">
         <div className="flex-1">
-          <img src={SolarPanelModuleImg} alt="" className="h-80 mx-auto pt-2" />
+          <img src={SolarPanelModuleImg} alt="" className="h-80 mx-auto" />
         </div>
-        <div className="flex-1">
-          <div className="flex flex-col w-8/12 mx-auto">
+        <div className="w-8/12">
+          <div className="flex flex-col">
             <h1 className="text-2xl mb-4 font-bold text-center">
-              Lorem, ipsum.
+              Solar Modules
             </h1>
             <p className="px-4">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Voluptas, fugit! Dolore illum fuga, culpa repudiandae nulla
-              corrupti provident dolores doloribus.Lorem ipsum dolor sit amet
-              consectetur, adipisicing elit. Velit accusamus dolor possimus
-              quos!
+              Our solar panels are certified or tested to most
+              reputed International standards such as TUV, UL, IEC, CE, MNRE,
+              Clean Energy Council, etc They have strong mechanical strength and
+              are certified to withstand high wind loads and snow loads while
+              being extremely responsive in low light conditions capturing more
+              sunlight in cloudy conditions ensuring better power generation.
+              The glass used in these solar panels are having ARC coatings. We
+              offer solar PV panels in both mono crystalline and polycrystalline
+              technologies in various wattage and cell configurations. They are
+              based on latest technologies
             </p>
           </div>
         </div>
@@ -29,16 +34,15 @@ const SolarModule = () => {
 
       <div className="flex p-6 gap-x-4">
         <div className="w-3/12">
-          <MultiLevelSidebar/>
+          <MultiLevelSidebar />
         </div>
         <div className="flex flex-wrap  gap-4 w-9/12">
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
         </div>
       </div>
     </>
