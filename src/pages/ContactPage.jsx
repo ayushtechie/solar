6debc7bc1614940b49a34b031f307a77ff969279
ayushtechie <techie.ayushtiwari@gmail.com>
2024-03-18 03:12:39 +0530

    edit contact component

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -4,10 +4,37 @@ import {
   MapPinIcon,
 } from "@heroicons/react/24/outline";
 import { DevicePhoneMobileIcon } from "@heroicons/react/24/outline";
+import IndianMapSVG from "../components/IndianMap";
+
+
+
 const ContactPage = () => {
   return (
     <>
-      <div className="bg-[url('https://res.cloudinary.com/dtyhpa19d/image/upload/v1708361078/SolarShopSolutions/industry-factory-area-solar-cells-electric-green-energy-solar-panels-line-view_44353-1405_yca3qe.jpg')] bg-no-repeat bg-center bg-cover flex gap-4 px-32 py-10">
+      <div className="sm:flex h-[calc(100vh-80px)] items-center">
+        <div className="flex-1">
+<IndianMapSVG/>
+        </div>
+        <div className="flex-1">
+          <h1 className="text-center uppercase font-bold sm:text-[2.4vw]">
+            Contact Us
+          </h1>
+          <h3 className="text-xl text-center text-gray-700">
+            SolarShopSolutions is the leading solar energy marketplace of <br />
+            India helping industries shift to solar power more easily. <br />
+            Contact us today to get a solar power plant installed at your site.
+          </h3>
+          <form className="w-[70%] mt-2 m-auto">
+            <input className="block w-full mt-2 rounded-lg" type="text" name="name" id="name" placeholder="Name"/>
+            <input className="block w-full mt-2 rounded-lg" type="tel" name="phone" id="phone" placeholder="Phone No"/>
+            <input className="block w-full mt-2 rounded-lg" type="text" name="cname" id="cname" placeholder="Company"/>
+            <input className="block w-full mt-2 rounded-lg" type="email" name="email" id="email" placeholder="Email"/>
+            <textarea className="block w-full mt-2 rounded-lg" placeholder="Comment" name="comment" id="comment" cols="30" rows="1"></textarea>
+            <button className="block mt-2 py-1 rounded-lg m-auto bg-black text-white w-2/6" type="submit">Get in tuch</button>
+          </form>
+        </div>
+      </div>
+      {/* <div className="bg-[url('https://res.cloudinary.com/dtyhpa19d/image/upload/v1708361078/SolarShopSolutions/industry-factory-area-solar-cells-electric-green-energy-solar-panels-line-view_44353-1405_yca3qe.jpg')] bg-no-repeat bg-center bg-cover flex gap-4 px-32 py-10">
         <div className="flex-auto w-6/12 bg-[#337037ab] p-12 rounded">
           <p className="text-xl text-white">CONTACT US</p>
           <p className="text-3xl text-white">Get in Touch</p>
@@ -47,7 +74,7 @@ const ContactPage = () => {
             <button type="submit" className="self-center mt-2 bg-green-700 text-white py-2 px-4 rounded-lg">Submit</button>
           </form>
         </div>
-        <div className="flex-1 p-2 bg-[#ffd75ea3] text-[#6e5b21]">
+        <div className="flex-1 p-2 bg-[#ffde7ca2] text-[#337037]">
           <h2 className="text-3xl font-semibold">INFO</h2>
 
           <div className="flex flex-col gap-2">
@@ -92,70 +119,14 @@ const ContactPage = () => {
           </div>
         </div>
       </div>
-
-      <div className="flex">
-        <form action="" className="pl-48 pr-96 py-24 w-4/5">
-          <h2 className="text-4xl font-semibold tracking-wider">Contact Us</h2>
-          <p className="text-base px-2 py-1 text-gray-700 tracking-wider mb-8">
-            Feel Free to contact us any time. We will get back to you as soon as
-            we can!.
-          </p>
-          <input
-            className="text-base px-2 py-1 block mb-2 w-full focus:outline-none focus:border-b-green-900 border-b-2"
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Name"
-          />
-          <input
-            className="text-base px-2 py-1 block mb-2 w-full border-b-2 focus:outline-none focus:border-b-green-900"
-            type="email"
-            name="email"
-            id="email"
-            placeholder="email"
-          />
-          <input
-            className="text-base px-2 py-1 block mb-2 w-full border-b-2 focus:outline-none focus:border-b-green-900"
-            type="tel"
-            name=""
-            id=""
-            placeholder="Phone no"
-          />
-          <textarea
-            className="mb-4 text-base px-2 py-1 block mb-2 w-full border-b-2 focus:outline-none focus:border-b-green-900"
-            name="msg"
-            id="msg"
-            cols="30"
-            rows="2"
-            placeholder="Message"
-          ></textarea>
-          <button
-            type="submit"
-            className="bg-green-600 text-white py-2 px-4 rounded-full"
-          >
-            Send
-          </button>
-        </form>
-        <div className="bg-gradient-to-r from-green-300 to-orange-100 w-1/5"></div>
-      </div>
-      <div className=" right-36 bg-black rounded-l-[25px] text-white top-36 h-96 w-96 p-5">
-        <h2 className="text-3xl font-semibold tracking wider my-2">Info</h2>
-        <a className="block my-2" href="tel:+918447705400">
-          <DevicePhoneMobileIcon className="w-8 h-8" />
-          +918447705400
-        </a>
-        <a className="block my-2" href="mailto:">
-          <EnvelopeIcon className="w-16 h-16" /> mayank@solarshopsolutions.in
-        </a>
-        <iframe
+      <iframe
           src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14028.440577532183!2d77.0823455!3d28.4762294!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d19e512a51995%3A0xd8399df4d2423de8!2ssolar%20shop%20solutions!5e0!3m2!1sen!2sin!4v1708371299775!5m2!1sen!2sin"
           width="340"
           height="220"
           allowfullscreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
-        ></iframe>
-      </div>
+        ></iframe> */}
     </>
   );
 };
